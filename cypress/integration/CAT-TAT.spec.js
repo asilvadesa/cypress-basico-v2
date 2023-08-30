aafd923ff8d3606cceee04e0b78a2805022451f8
Anderson Silva de Sa <asilvadesa@gmail.com>
2023-08-29 23:34:43 -0300

    Add save new person test

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,8 +1,22 @@
 ///<reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('Verifica o título da aplicação', function() {
+
+    beforeEach(function(){
         cy.visit('./src/index.html')
+    })
+
+    it('Verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('Preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Anderson')
+        cy.get('#lastName').type('Sá')
+        cy.get('#email').type('andsa@mock.com')
+        cy.get('#open-text-area').type('Basic Test')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
 })
