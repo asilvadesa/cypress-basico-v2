75974e452d50078268ebc5b652c7a899271a5286
Anderson Silva de Sa <asilvadesa@gmail.com>
2023-08-30 21:02:10 -0300

    Add delay

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -11,10 +11,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it.only('Preenche os campos obrigatórios e envia o formulário', function() {
+        
+        const longText = 'Esse campo é um item obrigatório e por isso deve ser preenchido e para testa essa funcionalidade tenho que digitar um grande número de carateres.'
+        
         cy.get('#firstName').type('Anderson')
         cy.get('#lastName').type('Sá')
         cy.get('#email').type('andsa@mock.com')
-        cy.get('#open-text-area').type('Basic Test')
+        cy.get('#open-text-area').type(longText, {delay:0})
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
