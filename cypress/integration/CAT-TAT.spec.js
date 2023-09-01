42dc0d4a9064e075eec6af6ada7b1deaca4794e8
Anderson Silva de Sa <asilvadesa@gmail.com>
2023-08-31 23:14:27 -0300

    Add selection  suspension field

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -60,7 +60,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 
-    it.only('envia o formulario com sucesso usando seletor contains', function(){
+    it('envia o formulario com sucesso usando seletor contains', function(){
         cy.get('#firstName').type('Anderson')
         cy.get('#lastName').type('Sá')
         cy.get('#email').type('andsa@mock.com')
@@ -68,5 +68,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (Youtube) por seu texto', function() {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) pelo seu valor', function() {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it.only('seleciona um produto (Blog) pelo seu indice', function() {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
     })
 })
